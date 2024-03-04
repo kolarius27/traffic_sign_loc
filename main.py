43e0bf1f8ed6ff50db69fed2d31d2935bf4a1976
kolarius27 <michalkolar27@gmail.com>
2024-03-04 16:50:59 +0100

    comments added

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import json
 import time
 from os.path import join, isfile
 import csv
+import math
 
 """
 PATH TO OpenSfM: ADD ABSOLUTE PATH TO THE INSTALLATION FOLDER OF OpenSfM LIBRARY.
@@ -37,24 +38,63 @@ def append_track_row(row, tracks):
     a_series = pd.Series(row, index=tracks.columns)
     tracks.append(a_series, ignore_index=True)
 
+def get_theta_phi( _x, _y, _z):
+    dv = math.sqrt(_x*_x + _y*_y + _z*_z)
+    x = _x/dv
+    y = _y/dv
+    z = _z/dv
+    theta = math.atan2(y, x)
+    phi = math.asin(z)
+    return theta, phi
+
+
+# x,y position in cubemap
+# cw  cube width
+# W,H size of equirectangular image
+def map_cube(x, y, side, cw, W, H):
+
+    u = 2*(float(x)/cw - 0.5)
+    v = 2*(float(y)/cw - 0.5)
+
+    if side == "front":
+        theta, phi = get_theta_phi( 1, u, v )
+    elif side == "right":
+        theta, phi = get_theta_phi( -u, 1, v )
+    elif side == "left":
+        theta, phi = get_theta_phi( u, -1, v )
+    elif side == "back":
+        theta, phi = get_theta_phi( -1, -u, v )
+    elif side == "bottom":
+        theta, phi = get_theta_phi( -v, u, 1 )
+    elif side == "top":
+        theta, phi = get_theta_phi( v, u, -1 )
+
+    _u = 0.5+0.5*(theta/math.pi)
+    _v = 0.5+(phi/math.pi)
+    return _u*W,  _v*H
+
 
 if __name__ == '__main__':
     start = time.time()
 
+    # paths to bat files with opensfm steps
     bat_prepare = os.path.join(opensfm_path, 'bin\opensfm_prepare.bat')
     bat_reconstruct = os.path.join(opensfm_path, 'bin\opensfm_reconstruct.bat')
-
+    
+    # prepare list of images
     img_path = os.path.join(dataset_path, 'images')
     img_files = [f for f in os.listdir(img_path) if isfile(join(img_path, f))]
 
+    # load image coords of traffic sign
     pixel_coords_path = os.path.join(dataset_path, 'test_pano.csv')
     pixel_coords = pd.read_csv(pixel_coords_path, delimiter=';')
     pixel_coords.apply(pd.to_numeric, errors='coerce').fillna(pixel_coords)
-    print(dataset_path)
-
+    
+    # run preparation of tracks
     subprocess.run([bat_prepare, dataset_path])
     print('Tracks prepared')
 
+    # add tracks of traffic sign into tracks.csv
     tracks_path = os.path.join(dataset_path, 'tracks.csv')
     with open(tracks_path, 'a', newline='') as csvfile:
         tracks = csv.writer(csvfile, delimiter='\t')
@@ -63,15 +103,17 @@ if __name__ == '__main__':
             track_row = prepare_track_row(p_row)
             tracks.writerow(track_row)
 
+    # run reconstruction process
     subprocess.run([bat_reconstruct, dataset_path])
 
+    # load reconstruction file with geo-coordinates
     reconstruction_path = os.path.join(dataset_path, 'reconstruction.geocoords.json')
     reconstruction = open(reconstruction_path)
     reconstruction_json = json.loads(reconstruction.read())
 
-
-    tsign_coords = reconstruction_json
-    print(reconstruction_json[0]['points']['99999']['coordinates'])
+    # search for geo-coordinate of the traffic sign
+    tsign_coords = reconstruction_json[0]['points']['99999']['coordinates']
+    print(tsign_coords)
 
     reconstruction.close()
 
